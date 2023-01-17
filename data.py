7b58af7c3f3d5cb1b8b3ef23eb7fb1d0067c2fcb
ppopow <pawel.popow@gmail.com>
2023-01-17 14:32:13 +0000

    first commit

---
+++ data.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    # systemie dziesiętnym
+    print(10)
+    # dwójkowym (przedrostek 0b)
+    print(0b1010)
+    # ósemkowym (przedrostek 0o)
+    print(0o12)
+    # szesnastkowym (przedrostek 0x)
+    print(0xA)
